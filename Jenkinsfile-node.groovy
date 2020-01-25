b99336f6bed0961a69b10ba2f9c6970730601615
Guang Su <gwsu2008@gmail.com>
2020-01-25 11:46:31 -0800

    post

--- Jenkinsfile-node.groovy
+++ Jenkinsfile-node.groovy
@@ -77,14 +77,18 @@ node ('master') {
         } catch (Exception e) {
             currentBuild.result = 'FAILURE'
             throw e
-        }
-        if (currentBuild.result == 'UNSTABLE') {
-            echo 'I am UNSTABLE :/'
-        } else if (currentBuild.result == 'FAILURE') {
-            echo 'I am FAILURE :/'
-        }
-        else {
-            echo 'I have finished'
+        } finally {
+            def currentResult = currentBuild.result ?: 'SUCCESS'
+            def previousResult = currentBuild.getPreviousBuild()?.result
+            if (currentResult == 'UNSTABLE') {
+                echo 'This will run only if the run was marked as unstable'
+            }
+            
+            if (previousResult != null && previousResult != currentResult) {
+                echo 'This will run only if the state of the Pipeline has changed'
+                echo 'For example, if the Pipeline was previously failing but is now successful'
+            }
+            echo 'This will always run'
         }
     }
 }
